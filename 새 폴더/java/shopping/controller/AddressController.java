ad50e4bdd6f08d92e2581c79957d0590dfdb2704
slswk59 <120429305+slswk59@users.noreply.github.com>
2023-03-23 10:19:32 +0900

    Add files via upload

--- 새 폴더/java/shopping/controller/AddressController.java
+++ 새 폴더/java/shopping/controller/AddressController.java
@@ -5,18 +5,16 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import member.dto.AuthInfo;
 import shopping.dto.AddressDTO;
-import shopping.dto.PageDTO;
 import shopping.service.AddressService;
 
 //http://localhost:8090/myapp/shopping/delivery.do
-//http://localhost:8090/myapp/member/signup.do
 
 @Controller
 public class AddressController {
@@ -31,21 +29,45 @@ public class AddressController {
 		this.addressService = addressService;
 	}
 	
-	@RequestMapping("/shopping/delivery.do") // 받는 것 //보내주는 것
-	public ModelAndView listExecute(@ModelAttribute("pv") PageDTO pv, ModelAndView mav) {
-		System.out.println("pv:" + pv.getCurrentPage());
-		int totalRecord = AddressService.countProcess();
-		if (totalRecord >= 1) {
-			if (pv.getCurrentPage() == 0)
-				pv.setCurrentPage(1);
-
-			this.pdto = new PageDTO(pv.getCurrentPage(), totalRecord);
-			mav.addObject("pv", this.pdto);
-			mav.addObject("aList", AddressService.listProcess(this.pdto));
+//	// 배송지 조회
+//	@RequestMapping("/shopping/delivery.do") // 받는 것 //보내주는 것
+//	public ModelAndView listExecute(@ModelAttribute("pv") String pv, ModelAndView mav) {
+//		List<AddressDTO> aList = addressService.selectAllAddressesProcess(pv);
+//	    mav.addObject("aList", aList);
+//		mav.setViewName("shopping/delivery");
+//		return mav;
+//	}
+	
+	    // 배송지 조회
+		@RequestMapping("/shopping/delivery.do") // 받는 것 //보내주는 것
+		public ModelAndView listExecute(HttpSession session, ModelAndView mav) {
+			AuthInfo authInfo =(AuthInfo)session.getAttribute("authInfo");
+			System.out.println(authInfo.getId());
+			List<AddressDTO> aList = addressService.selectAllAddressesProcess(authInfo.getId());
+		    mav.addObject("aList", aList);
+			mav.setViewName("shopping/delivery");
+			return mav;
 		}
-		mav.setViewName("shopping/delivery");
-		return mav;
-	}
+		
+		
+		// 배송지 삭제
+		@RequestMapping(value="/shopping/delivery/deleteAddress.do", method=RequestMethod.POST)
+		public ModelAndView deleteAddressProcess(@RequestParam("del_key") int del_key, HttpSession session, ModelAndView mav) {
+		    AuthInfo authInfo = (AuthInfo)session.getAttribute("authInfo");
+		    addressService.deleteAddressProcess(del_key, authInfo.getId());
+		    mav.setViewName("redirect:/shopping/delivery.do");
+		    return mav;
+		}
+
+		
+		
+	// 배송지 수정
+	
+	
+	
+	
+		
+	// 배송지 추가
 	
 	
 
