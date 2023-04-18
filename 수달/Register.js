f42a9461bc51f7516d2ba442210e382fc1bcfd54
slswk59 <120429305+slswk59@users.noreply.github.com>
2023-04-18 12:29:13 +0900

    Add files via upload

---
+++ 수달/Register.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { useState } from "react";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  Form,
+  Input,
+  Container,
+  Row,
+  Col,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  FormGroup,
+  Label,
+} from "reactstrap";
+
+function Register() {
+  document.documentElement.classList.remove("nav-open");
+  React.useEffect(() => {
+    document.body.classList.add("register-page");
+    return function cleanup() {
+      document.body.classList.remove("register-page");
+    };
+  });
+  return (
+    <>
+      {/* <IndexNavbar /> */}
+      <div
+        className="page-header"
+        style={{
+          backgroundImage: "url(" + require("assets/img/login-image.jpg") + ")",
+        }}
+      >
+        <div className="filter" />
+        <Container>
+          <Row>
+            <Col className="ml-auto mr-auto" lg="4">
+              <Card className="card-register ml-auto mr-auto">
+                <h3 className="title mx-auto">회원가입</h3>
+                <div className="social-line text-center"></div>
+                <Form className="register-form">
+                  <label>이름</label>
+                  <InputGroup className="form-group-no-border">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="nc-icon nc-email-85" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Name" type="name" />
+                  </InputGroup>
+                  <label>성별</label>
+                  <InputGroup className="form-group-no-border">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="nc-icon nc-email-85" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Gender" type="gender" />
+                  </InputGroup>
+                  <label>닉네임</label>
+                  <InputGroup className="form-group-no-border">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="nc-icon nc-email-85" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Nickname" type="nickname" />
+                  </InputGroup>
+                  <label>이메일</label>
+                  <InputGroup className="form-group-no-border">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="nc-icon nc-email-85" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Email" type="email" />
+                  </InputGroup>
+                  <label>비밀번호</label>
+                  <InputGroup className="form-group-no-border">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="nc-icon nc-key-25" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Password" type="password" />
+                  </InputGroup>
+                  <Button block className="btn-round" color="danger">
+                    회원가입
+                  </Button>
+                </Form>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+        <div className="footer register-footer text-center">
+          <h6>
+            © {new Date().getFullYear()}, made with{" "}
+            <i className="fa fa-heart heart" /> by Creative Tim
+          </h6>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Register;
